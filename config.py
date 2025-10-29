4926f01f2ef0083f52bbe3ec270499e5d20b009c
Yeah-Nah <alexandra.helsham@gmail.com>
2025-10-29 12:35:14 +1100

    refactor ♻️: using logger function and cleaning up some code

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 # File paths - UPDATE THESE TO MATCH YOUR FILES
-INPUT_VIDEO_PATH = "input/sample_boat_video.mp4"  # Change this to your video file
-OUTPUT_VIDEO_PATH = "output/tracked_boats.mp4" 
+VIDEO_ROOT_FOLDER_PATH = "E:\\01_personal_project"
+VIDEO_FILE = "20251019 Broken Balmain.mp4"
+INPUT_VIDEO_PATH = f"{VIDEO_ROOT_FOLDER_PATH}\\input\\{VIDEO_FILE}"  # Change this to your video file
+OUTPUT_FOLDER_PATH = f"{VIDEO_ROOT_FOLDER_PATH}\\output\\"
+
+OUTPUT_FILE_SUFFIX = "_tracked"  # Suffix for output file names
