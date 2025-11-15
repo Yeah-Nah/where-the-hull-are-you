f83d86adddb31a7b2df9fc10dfbaaf62090326d9
Yeah-Nah <alexandra.helsham@gmail.com>
2025-11-15 15:05:32 +1100

    chore ♻️: moving files around

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # File paths - UPDATE THESE TO MATCH YOUR FILES
 VIDEO_ROOT_FOLDER_PATH = "E:\\01_personal_project"
-VIDEO_FILE = "20251019 Broken Balmain.mp4"
+VIDEO_FILE = "20251026_155600.mp4"
 INPUT_VIDEO_PATH = f"{VIDEO_ROOT_FOLDER_PATH}\\input\\{VIDEO_FILE}"  # Change this to your video file
 OUTPUT_FOLDER_PATH = f"{VIDEO_ROOT_FOLDER_PATH}\\output\\"
 
